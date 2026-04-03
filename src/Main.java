59a03bc7683d9929061f231762f6d1c0fa28393a
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-04-03 13:33:04 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static double simpleSmoothing(int numerator, int denominator, int classes) {
+        return (double) (numerator + 1) / (denominator + classes);
+    }
+}
